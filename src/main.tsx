144b05cfeb3f86c241594c462e389ca8f9366fea
Grom <k0nstamtinus@gmail.com>
2023-03-19 02:23:53 +0600

    Change manifestUrl on directly ref to GitHub

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'http://k0nstantini.github.io/Jetton-webapp/tonconnect-manifest.json';
+const manifestUrl = 'https://raw.githubusercontent.com/K0nstantini/Jetton-webapp/gh-pages/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
